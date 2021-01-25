d0705c53974646db3ea2e9093df120f4bcdfda89
KALININ ALEXEY <56075795+lelikone777@users.noreply.github.com>
2021-01-25 21:14:08 +0300

    1

---
+++ HouseVop2/assets/js/script.js
@@ -0,0 +1,20 @@
+document.querySelector('.header-menu-burger').addEventListener('click', function(){
+    document.querySelector('.header-menu-burger span').classList.toggle('active');
+})
+
+
+
+
+
+
+
+
+// document.querySelector('.header-menu-toggle').classList.toggle("animate")
+
+// document.getElementById("header-menu-burger").onclick = function() {
+//     open()
+// };
+
+// function open() {
+//     document.getElementById("menu").classList.toggle("show");
+// }
